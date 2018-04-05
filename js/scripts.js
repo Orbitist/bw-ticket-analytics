7308d7412119a71536297b200f125da2dca152db
Nicholas Gunner <support@getbeyondwords.com>
2018-04-04 21:13:47 -0600

    in the last three..

--- js/scripts.js
+++ js/scripts.js
@@ -63,7 +63,7 @@ function getTickets() {
 
     var movingAverage = ((thisMonthsTime + lastMonthsTime + monthBeforeLastMonthsTime)/3).toFixed(2);
 
-    $( "#total-hours" ).append("<p class='lead'>We've spent <strong>" + timeTotal + "</strong> billable hours supporting <strong>" + data.length + "</strong> tickets for <strong>" + member);
+    $( "#total-hours" ).append("<p class='lead'>We've spent <strong>" + timeTotal + "</strong> billable hours supporting <strong>" + data.length + "</strong> tickets for <strong>" + member + " in the last three months.");
     $( "#total-hours" ).append("<h1>" + monthBeforeLastMonth + ": " + monthBeforeLastMonthsTime + " billable hours<br>" + lastMonth + ": " + lastMonthsTime + " billable hours<br>" + thisMonth + ": " + thisMonthsTime + " billable hours</h1>");
     $( "#total-hours" ).append("<h1>3-month average: " + movingAverage + " billable hours</h1>");
   });
